7c0e1f6ffbc7d65336c12293109fd187f7af7922
asad059 <asadshahid930@gmail.com>
2023-11-27 16:18:54 +0000

    Automated update to test.txt

--- push.sh
+++ push.sh
@@ -6,6 +6,9 @@ FILE_PATH="test.txt"
 UPDATE_TEXT="This is an automated update."
 COMMIT_MESSAGE="Automated update to $FILE_PATH"
 
+# Personal Access Token (replace YOUR_TOKEN with your actual token)
+TOKEN="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"
+
 # Change directory to the Git repository
 cd "$REPO_PATH" || { echo "Error: Unable to change directory to $REPO_PATH"; exit 1; }
 
@@ -17,4 +20,5 @@ git add .
 git commit -m "$COMMIT_MESSAGE"
 
 # Push changes to the remote repository
-git push origin main
+#git push origin main
+git push https://asad059:$TOKEN@github.com/asad059/iac-ec2.git main
